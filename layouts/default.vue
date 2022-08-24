e76ccc5eac54db1dc3dd74a60aa67d3d130164b5
christianmarzian <46906798+christianmarzian@users.noreply.github.com>
2022-08-24 16:32:51 +0200

    Help Beacon

--- layouts/default.vue
+++ layouts/default.vue
@@ -63,6 +63,8 @@
         </div>
       </div>
     </footer>
+    <script type="text/javascript">!function(e,t,n){function a(){var e=t.getElementsByTagName("script")[0],n=t.createElement("script");n.type="text/javascript",n.async=!0,n.src="https://beacon-v2.helpscout.net",e.parentNode.insertBefore(n,e)}if(e.Beacon=n=function(t,n,a){e.Beacon.readyQueue.push({method:t,options:n,data:a})},n.readyQueue=[],"complete"===t.readyState)return a();e.attachEvent?e.attachEvent("onload",a):e.addEventListener("load",a,!1)}(window,document,window.Beacon||function(){});
+    </script><script type="text/javascript">window.Beacon('init', 'c873c74a-8b69-496d-9416-364983a832c7')</script>
   </div>
 </template>
 
